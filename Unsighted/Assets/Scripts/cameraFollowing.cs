99b7c792e015c29e6683784c16fcdf033deaf71a
DimuthuLakmal <kjtdimuthu@gmail.com>
2016-12-01 01:06:18 +0530

    Prince character added. And parallax scrolling movement for backgrounds added

---
+++ Unsighted/Assets/Scripts/cameraFollowing.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollowing : MonoBehaviour {
+
+    public Transform target; //what camera is following
+    public float smoothing; //dampening effect
+
+    Vector3 offset;
+
+    float lowY;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - target.position;
+        lowY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 targetCamPos = target.position + offset;
+
+            transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+            if (transform.position.y < lowY)
+            {
+                transform.position = new Vector3(transform.position.x, lowY, transform.position.z);
+            }
+        }
+
+    }
+}
